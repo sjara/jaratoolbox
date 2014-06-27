bba1891ac6685813dba5cd9b7d562a58667adcad
Santiago Jaramillo <sjara@eon.(none)>
2014-06-27 10:56:54 -0700

    Added init file.

---
+++ __init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+'''
+"jaratoolbox" is a set of python modules for the analysis of behavioral and electrophysiological data.
+
+It is developed by the Jaramillo Lab at the Institute of Neuroscience, University of Oregon.
+http://jaralab.uoregon.edu/
+'''
+
