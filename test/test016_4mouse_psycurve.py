40a3cd80aee2627aedbda9f4801674ce0209b26a
Nick Ponvert <nickponvert@gmail.com>
2014-11-18 16:16:50 -0800

    Added errorbars to 4-mouse psycurve

--- test/test016_4mouse_psycurve.py
+++ test/test016_4mouse_psycurve.py
@@ -4,6 +4,7 @@ from jaratoolbox import loadbehavior
 import numpy as np
 from pylab import *
 import matplotlib.pyplot as plt
+from statsmodels.stats.proportion import proportion_confint #Used to compute confidence interval for the error bars. 
 
 subjects = ['hm4d004', 'hm4d005', 'hm4d006']
 session = '20140826a'
@@ -28,6 +29,7 @@ for ind, subject in enumerate(subjects):
     possibleFreq = np.unique(targetFrequency)
     nFreq = len(possibleFreq) 
     fractionRightEachFreq=np.zeros(nFreq)
+    confLimitsEachFreq=np.zeros([2, nFreq]) #Upper and lower confidence limitsi
     trialsEachFreq = behavioranalysis.find_trials_each_type(targetFrequency,possibleFreq)
 
     positions=[(0,0), (0,1), (1,0), (1,1)]
@@ -36,14 +38,24 @@ for ind, subject in enumerate(subjects):
     nTrialsEachFreq = np.empty(nFreq)
     nRightwardEachFreq = np.empty(nFreq)
     for indf,thisFreq in enumerate(possibleFreq):
-        nTrialsEachFreq[indf] = sum(valid & trialsEachFreq[:,indf])
-        nRightwardEachFreq[indf] = sum(valid & choiceRight & trialsEachFreq[:,indf])
+
+        nTrialsThisFreq = sum(valid & trialsEachFreq[:,indf])
+        nRightwardThisFreq =  sum(valid & choiceRight & trialsEachFreq[:,indf])
+        conf = np.array(proportion_confint(nRightwardThisFreq, nTrialsThisFreq, method = 'wilson'))
+        
+        nTrialsEachFreq[indf] = nTrialsThisFreq
+        nRightwardEachFreq[indf] = nRightwardThisFreq
+        confLimitsEachFreq[0, indf] = conf[0] # Lower ci
+        confLimitsEachFreq[1, indf] = conf[1] # Upper ci
 
     fractionRightEachFreq = nRightwardEachFreq/nTrialsEachFreq.astype(float)
 
     #plot(possibleFreq,fractionRightEachFreq,'-o')
     #gca().set_xscale('log')
-    ax1.plot(fractionRightEachFreq,'-o')
+    upperWhisker = confLimitsEachFreq[1, :] - fractionRightEachFreq #Find lengths of whiskers for plotting function
+    lowerWhisker = fractionRightEachFreq - confLimitsEachFreq[0,:]
+    x = range(nFreq)
+    errorbar(x, fractionRightEachFreq, yerr = [lowerWhisker, upperWhisker])
     axhline(y=0.5, color = 'red')
     title(subject)
     ylim([0,1])
