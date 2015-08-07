e9277ac0e21f76db6886df7cd45596e9ec14c5ff
Santiago Jaramillo <sjara@uoregon.edu>
2015-08-07 11:45:00 -0700

    Added FlipThrough class.

--- extraplots.py
+++ extraplots.py
@@ -197,3 +197,53 @@ def plot_psychometric(possibleValues,fractionHitsEachValue,ciHitsEachValue=None,
     return (pline, pcaps, pbars, pdots)
 
 
+class FlipThrough(object):
+    def __init__(self, plotter, data):
+        '''
+        Allow flipping through data plots.
+        Args:
+            plotter (function): function that plots data.
+            data (list): list of tuples containing the parameters for the plotter function.
+        '''
+        self.plotter = plotter
+        self.data = data
+        self.counter = 0
+        self.maxDataInd = len(data)-1
+        self.fig = plt.gcf()
+        self.redraw() # Plot data
+        self.kpid = self.fig.canvas.mpl_connect('key_press_event', self.on_key_press)
+
+    def redraw(self):
+        self.fig.clf()
+        if isinstance(self.data[self.counter], tuple):
+            # FIXME: this will fail if the function requires a tuple as input
+            self.plotter(*self.data[self.counter])
+        else:
+            self.plotter(self.data[self.counter])
+        plt.suptitle('{}/{}: Press < or > to flip through data'.format(self.counter+1,
+                                                                       self.maxDataInd+1))
+        self.fig.show()
+
+    def on_key_press(self, event):
+        '''
+        Method to listen for keypresses and flip to next/previous view
+        '''
+        if event.key=='<' or event.key==',' or event.key=='left':
+            if self.counter>0:
+                self.counter -= 1
+            else:
+                self.counter = self.maxDataInd
+            self.redraw()
+        elif event.key=='>' or event.key=='.' or event.key=='right':
+            if self.counter<self.maxDataInd:
+                self.counter += 1
+            else:
+                self.counter = 0
+            self.redraw()
+
+
+if __name__=='__main__':
+    rdata = np.random.randint(0,9,(10,3,3))
+    dataList = [(m,) for m in rdata]
+    ft=FlipThrough(plt.imshow,dataList)
+
