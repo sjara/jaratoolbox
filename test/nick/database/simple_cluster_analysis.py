cce4a500aa21e03564ffb14d723f9012ab59abfb
Nick Ponvert <nickponvert@gmail.com>
2015-08-11 20:26:52 -0700

    Simple spike selector and cluster analysis

---
+++ test/nick/database/simple_cluster_analysis.py
@@ -0,0 +1,50 @@
+from jaratoolbox.test.nick.database import dataloader
+reload(dataloader)
+
+from jaratoolbox.test.nick.database import cellDB
+reload(cellDB)
+
+from jaratoolbox.test.nick.database import dataplotter
+from jaratoolbox import spikesorting
+from jaratoolbox.test.nick import clustercutting
+
+from pylab import *
+
+
+loader = dataloader.DataLoader('offline', experimenter='nick')
+
+dbFn = '/home/nick/data/database/nick_thalamus_cells.json'
+db = cellDB.CellDB()
+db.load_from_json(dbFn)
+
+
+c = db[8]
+spikeData, eventData, behavData = loader.get_cluster_data(c, 'laserPulse')
+
+spikeTimes = spikeData.timestamps
+eventOnsetTimes = loader.get_event_onset_times(eventData)
+
+# figure()
+# spikesorting.plot_waveforms(spikeData.samples)
+# title('all spikes')
+
+# figure()
+# dataplotter.plot_raster(spikeTimes, eventOnsetTimes)
+# show()
+
+# fet = spikesorting.calculate_features(spikeData.samples, ['peak', 'valley', 'energy'])
+fet = spikesorting.calculate_features(spikeData.samples, ['peak'])
+
+cw = clustercutting.ClusterCutter(fet)
+
+inp = None
+while inp==None:
+    inp = raw_input("Press Enter to Continue")
+
+
+figure()
+spikesorting.plot_waveforms(spikeData.samples[cw.inCluster])
+title('in new cluster')
+
+
+
