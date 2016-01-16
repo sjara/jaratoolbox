7861addd503e5450828aabbe768d8ec655a0f8c8
Nick Ponvert <nickponvert@gmail.com>
2016-01-16 15:10:55 -0800

    Added nick behavior scripts

---
+++ test/nick/dailybehavior.py
@@ -0,0 +1,16 @@
+'''
+For Daily Behavior Monitoring.
+Loads behavior data from mounted jarahub/data/behavior for animals of interest, plot psychometric curve and dynamics data.
+'''
+import sys
+from jaratoolbox.test.nick import behavioranalysis_vnick as behavioranalysis
+reload(behavioranalysis)
+
+subjects = ['amod001', 'amod002', 'amod003', 'amod004', 'amod005']
+
+if len(sys.argv)>1:
+    sessions = sys.argv[1:]
+    #sessions = input("Enter sessions (in a list of strings ['','']) to check behavior performance:")
+
+behavioranalysis.behavior_summary(subjects,sessions,trialslim=[0,1000],outputDir='/home/nick/data/behavior_reports')
+
