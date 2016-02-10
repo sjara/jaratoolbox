e00792420c19c38db7cf331f8f5d5c57669b99d6
Nick Ponvert <nickponvert@gmail.com>
2016-02-09 20:43:05 -0800

    AUROC tests

--- test/nick/20160130_testAUROC.py
+++ test/nick/20160130_testAUROC.py
@@ -17,7 +17,7 @@ figdb.load_from_json(figdbFn)
 
 loader = dataloader.DataLoader('offline', experimenter='nick')
 
-figdb[1].get_session_types()
+figdb[0].get_session_types()
 
 clusterfuncs.plot_cluster_tuning(figdb[1], 3)
 
@@ -37,6 +37,179 @@ timeRange = [-0.5, 1]
 
 spikeTimesFromEventOnset, trialIndexForEachSpike, indexLimitsEachTrial = spikesanalysis.eventlocked_spiketimes(
     spikeTimestamps, eventOnsetTimes, timeRange)
+
+
+
+
+
+def num_spikes_in_timerange_select_trials(spikeTimestamps, eventOnsetTimes, timeRange, selectInds):
+    spikeTimesFromEventOnset, trialIndexForEachSpike, indexLimitsEachTrial = spikesanalysis.eventlocked_spiketimes(spikeTimestamps, eventOnsetTimes, timeRange)
+    numSpikesEachTrial=squeeze(diff(indexLimitsEachTrial, axis=0))
+    numSpikesSelectTrials=numSpikesEachTrial[selectInds]
+
+    return numSpikesSelectTrials
+
+
+
+
+
+#####################################################
+### Code to plot the auroc versus baseline for all times, all freqs
+from sklearn.metrics import roc_curve, auc
+
+starts = arange(-0.5, 1, 0.1)
+stops = arange(-0.4, 1.1, 0.1)
+ranges = vstack((starts, stops))
+
+auroc_mat = zeros([len(possibleFreq), len(starts)])
+
+for indFreq, freq in enumerate(possibleFreq):
+    select = np.flatnonzero((currentFreq==freq)&(currentIntensity==70))
+
+    baselineRange = [-0.3, -0.2]
+
+    spikesBaseline = num_spikes_in_timerange_select_trials(spikeTimestamps, eventOnsetTimes, baselineRange, select)
+
+    labelsBaseline = zeros(len(spikesBaseline))
+
+
+    for indRange in range(len(starts)):
+
+        spikesThisRange = num_spikes_in_timerange_select_trials(spikeTimestamps, eventOnsetTimes, ranges[:,indRange], select)
+        labelsThisRange = ones(len(spikesThisRange))
+
+        spikes = concatenate((spikesBaseline, spikesThisRange))
+        labels = concatenate((labelsBaseline, labelsThisRange))
+
+        fpr, tpr, thresholds = roc_curve(labels, spikes)
+        auroc = auc(fpr, tpr)
+
+        auroc_mat[indFreq, indRange] = auroc
+
+
+ax=subplot(111)
+cm = ax.imshow(auroc_mat, interpolation='none', cmap='bwr')
+cm.set_clim([0, 1])
+ax.set_yticklabels(possibleFreq/1000)
+
+#########################################################################
+
+#Now I want to plot the auroc versus the response to all other freqencies,
+#all at a single intensity
+
+auroc_mat = zeros((len(possibleFreq), len(possibleFreq)))
+
+responseRange = [0, 0.1]
+intensity = 70
+
+for indFreq1, freq1 in enumerate(possibleFreq):
+    selectFreq1 = np.flatnonzero((currentFreq==freq1)&(currentIntensity==intensity))
+
+    spikesFreq1 = num_spikes_in_timerange_select_trials(spikeTimestamps, eventOnsetTimes, responseRange, selectFreq1)
+    labelsFreq1 = ones(len(spikesFreq1)) #The first freq is the ones
+
+    for indFreq2, freq2 in enumerate(possibleFreq):
+        selectFreq2 = np.flatnonzero((currentFreq==freq2)&(currentIntensity==intensity))
+
+        spikesFreq2 = num_spikes_in_timerange_select_trials(spikeTimestamps, eventOnsetTimes, responseRange, selectFreq2)
+        labelsFreq2 = zeros(len(spikesFreq2)) #THe second freq is the zeros
+
+        spikes = concatenate((spikesFreq1, spikesFreq2))
+        labels = concatenate((labelsFreq1, labelsFreq2))
+
+        fpr, tpr, thresholds = roc_curve(labels, spikes)
+        auroc = auc(fpr, tpr)
+
+        auroc_mat[indFreq2, indFreq1] = auroc
+
+ax=subplot(111)
+cm = ax.imshow(auroc_mat, interpolation='none', cmap = 'bwr')
+colorbar(cm)
+cm.set_clim([0, 1])
+ax.set_yticks(range(len( possibleFreq )))
+ax.set_yticklabels(["{:2.1f}".format(x) for x in possibleFreq/1000])
+ax.set_xticks(range(len( possibleFreq )))
+ax.set_xticklabels(["{:2.1f}".format(x) for x in possibleFreq/1000], rotation='vertical')
+
+
+##############################################
+#Now I want to plot the auroc versus baseline for each frequency, each intensity
+
+
+
+auroc_mat = zeros([len(possibleIntensity), len(possibleFreq)])
+baselineRange = [-0.3, -0.2]
+responseRange = [0, 0.1]
+
+for indFreq, freq in enumerate(possibleFreq):
+    for indInten, intensity in enumerate(possibleIntensity):
+
+        select = np.flatnonzero((currentFreq==freq)&(currentIntensity==intensity))
+
+
+        spikesBaseline = num_spikes_in_timerange_select_trials(spikeTimestamps, eventOnsetTimes, baselineRange, select)
+        labelsBaseline = zeros(len(spikesBaseline))
+
+        spikesResponse = num_spikes_in_timerange_select_trials(spikeTimestamps, eventOnsetTimes, responseRange, select)
+        labelsResponse = ones(len(spikesResponse))
+
+
+        spikes = concatenate((spikesBaseline, spikesResponse))
+        labels = concatenate((labelsBaseline, labelsResponse))
+
+        fpr, tpr, thresholds = roc_curve(labels, spikes)
+        auroc = auc(fpr, tpr)
+
+        auroc_mat[indInten, indFreq] = auroc
+
+
+ax=subplot(111)
+cm = ax.imshow(flipud(auroc_mat), interpolation='none', cmap = 'RdGy', aspect='auto')
+colorbar(cm)
+cm.set_clim([0, 1])
+ax.set_yticks(range(len( possibleIntensity )))
+ax.set_yticklabels(["{:2.1f}".format(x) for x in possibleIntensity][::-1])
+ax.set_xticks(range(len( possibleFreq )))
+ax.set_xticklabels(["{:2.1f}".format(x) for x in possibleFreq/1000], rotation='vertical')
+
+
+###############
+#I now have code to test the responses through time, the responses relative to other freqs,
+#and the responses relative to different intensities (resp versus baseline)
+
+
+
+
+
+
+
+
+#DEBUG
+select = np.flatnonzero((currentFreq==2000)&(currentIntensity==70))
+
+baselineRange = [-0.3, -0.2]
+
+spikesBaseline = num_spikes_in_timerange_select_trials(spikeTimestamps, eventOnsetTimes, baselineRange, select)
+
+labelsBaseline = zeros(len(spikesBaseline))
+
+thisRange = ranges[:,0]
+
+spikesThisRange = num_spikes_in_timerange_select_trials(spikeTimestamps, eventOnsetTimes, thisRange, select)
+labelsThisRange = ones(len(spikesThisRange))
+
+spikes = concatenate((spikesBaseline, spikesThisRange))
+labels = concatenate((labelsBaseline, labelsThisRange))
+
+#CALCULATE AUROC AND PLOT!!
+fpr, tpr, thresholds = roc_curve(labels, spikes)
+auroc = auc(fpr, tpr)
+plot(fpr, tpr)
+
+
+
+
+
 
 plot(spikeTimesFromEventOnset, trialIndexForEachSpike, '.')
 
