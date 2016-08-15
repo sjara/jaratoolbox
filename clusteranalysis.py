5a46d2985b3b11c84c193f0706b8a22f2279f839
Nick Ponvert <nickponvert@gmail.com>
2016-08-15 11:35:52 -0700

    Nearly-working cluster analysis

--- clusteranalysis.py
+++ clusteranalysis.py
@@ -3,12 +3,16 @@ Analyze the quality of clusters produced by spike-sorting
 '''
 
 import numpy as np
+import os
 import matplotlib.pyplot as plt
 from jaratoolbox import spikesorting
-from jaratoolbox import celldatabase
+# from jaratoolbox import celldatabase
 from jaratoolbox import loadopenephys
-#from jaratoolbox import ephyscore
+from jaratoolbox import settings
+import itertools
+import pickle
 
+#from jaratoolbox import ephyscore
 
 def find_ephys_sessions(cellDB):
     return list(np.unique(cellDB.get_vector('ephysSession')))
@@ -20,7 +24,7 @@ def waveforms_many_sessions(subject, ephysSessions, tetrode, clustersPerTetrode=
     '''
     waveformsOneTetrode = []
     for oneSession in ephysSessions:
-        waves = calculate_avg_waveforms(subject, ephysSession, tetrode,
+        waves = calculate_avg_waveforms(subject, oneSession, tetrode,
                                 clustersPerTetrode=clustersPerTetrode, wavesize=wavesize)
         waveformsOneTetrode.append(waves)
     return waveformsOneTetrode
@@ -34,23 +38,28 @@ def calculate_avg_waveforms(subject, ephysSession, tetrode, clustersPerTetrode=1
     NOTE: This method is inefficient because it load the spikes file for each cluster.
     '''
 
-    # TODO: Load data for one tetrodes and calculate average for each cluster.
+    # DONE: Load data for one tetrodes and calculate average for each cluster.
     #ephysFilename = ???
-    #spikes = loadopenephys.DataSpikes(ephysFilename)
+    ephysDir = os.path.join(settings.EPHYS_PATH, subject, ephysSession)
+    ephysFilename = os.path.join(ephysDir, 'Tetrode{}.spikes'.format(tetrode))
+    spikes = loadopenephys.DataSpikes(ephysFilename)
 
-    # TODO: Load cluster file
+    # DONE: Load cluster file
     #kkDataDir = os.path.dirname(self.filename)+'_kk'
-    #clusterFilename = 'Tetrode{0}.clu.1'.format(tetrode)
     #fullPath = os.path.join(kkDataDir,clusterFilename)
-    #clusters = np.fromfile(fullPath,dtype='int32',sep=' ')[1:]
+    clustersDir = '{}_kk'.format(ephysDir)
+    clusterFilename = os.path.join(clustersDir, 'Tetrode{}.clu.1'.format(tetrode))
+    clusters = np.fromfile(clusterFilename, dtype='int32', sep=' ')[1:]
 
-    # TODO: loop through clusters
+    # DONE: loop through clusters
     allWaveforms = np.empty((clustersPerTetrode,wavesize))
     for indc in range(clustersPerTetrode):
-        print 'Estimating average waveform for {0} T{1}c{2}'.format(ephysSession,tetrode,indc)
+        print 'Estimating average waveform for {0} T{1}c{2}'.format(ephysSession,tetrode,indc+1)
 
-        # TODO: get waveforms for one cluster
-        # waveforms = ??????????
+        # DONE: get waveforms for one cluster
+        #Add 1 to the cluster index because clusters start from 1
+        waveforms = spikes.samples[clusters==indc+1, :, :]
+
         alignedWaveforms = spikesorting.align_waveforms(waveforms)
         meanWaveforms = np.mean(alignedWaveforms,axis=0)
         allWaveforms[indc,:] = meanWaveforms.flatten()
@@ -101,25 +110,126 @@ def spikeshape_correlation(waveforms):
     return (ccSelf,ccAccross)
 
 
-def plot_correlation(ccSelf,ccAccross):
+def plot_correlation(ccSelf,ccAccross,cmap='hot'):
     nSessions = len(ccSelf)
     plt.clf()
     for inds in range(nSessions):
         plt.subplot2grid((2,nSessions),(0,inds))
-        plt.imshow(ccSelf[inds],clim=[0,1], cmap='hot',interpolation='nearest')
+        plt.imshow(ccSelf[inds],clim=[0,1], cmap=cmap,interpolation='nearest')
         plt.axis('image')
         #title('')
     for inds in range(nSessions-1):
         plt.subplot2grid((2,nSessions-1),(1,inds))
-        plt.imshow(ccAccross[inds],clim=[0,1], cmap='hot',interpolation='nearest')
+        plt.imshow(ccAccross[inds],clim=[0,1], cmap=cmap,interpolation='nearest')
         plt.axis('image')
     plt.colorbar()
     plt.draw()
 
+def rsync_session_data(subject,
+                       session,
+                       serverUser = 'jarauser',
+                       serverName = 'jarastore',
+                       serverEphysPath = '/data2016/ephys',
+                       skipIfExists=False):
+    '''
+    Rsync just the sessions you need from jarahub
+    '''
+    fullRemotePath = os.path.join(serverEphysPath, subject, session)
+    serverDataPath = '{}@{}:{}'.format(serverUser, serverName, fullRemotePath)
+    localDataPath = os.path.join(settings.EPHYS_PATH, subject) + os.sep
+    fullLocalPath = os.path.join(localDataPath, session)
+    transferCommand = ['rsync', '-av', serverDataPath, localDataPath]
+    if skipIfExists:
+        if not os.path.exists(fullLocalPath):
+            subprocess.call(transferCommand)
+    else:
+        subprocess.call(transferCommand)
+
+
+def comparison_label_array(session1, session2, tetrode):
+    '''
+    Returns an array of strings that describe the comparisons made between two sessions
+    '''
+    clabs1 = ['{}_T{}c{}'.format(session1, tetrode, cnum) for cnum in range(1, 13)]
+    clabs2 = ['{}_T{}c{}'.format(session2, tetrode, cnum) for cnum in range(1, 13)]
+    labs = ['{} x {}'.format(cl1, cl2) for cl1, cl2 in itertools.product(clabs1, clabs2)]
+    larray = np.array(labs, dtype=str).reshape((12, 12))
+    return larray
+
+
+def session_good_clusters(cellDB, session, tetrode):
+    '''
+    Return a 12-item vector, containing 1 if the cluster was good quality and zero otherwise
+    '''
+    allcellsTetrode = cellDB.get_vector('tetrode')
+    allcellsSession = cellDB.get_vector('ephysSession')
+    allcellsQuality = cellDB.get_vector('quality')
+    #The cells to use come from this session, this tetrode
+    cellsThisSession = allcellsSession==session
+    cellsThisTetrode = allcellsTetrode==tetrode
+    cellsToUse = (cellsThisSession & cellsThisTetrode)
+    #Whether each cell in the db is good
+    allcellsGoodQuality = ((allcellsQuality==1) | (allcellsQuality==6))
+    #Whether the cells that we want to use are good
+    goodQualityToUse = allcellsGoodQuality[cellsToUse].astype(int)
+    #The cluster number for each cluster (in case not full 12, or does not start at 1)
+    allcellsClusterNumber = cellDB.get_vector('cluster')
+    clusterNumsToUse = allcellsClusterNumber[cellsToUse]
+    #Initialize to zero
+    passingClusters = np.zeros(12)
+    # Set the good quality clusters to 1
+    for indClust, clusterNum in np.ndenumerate(clusterNumsToUse):
+        passingClusters[clusterNum-1]=goodQualityToUse[indClust]
+    return passingClusters
+
+def comparison_quality_filter(cellDB, session1, session2, tetrode):
+    #DONE: FINISH THIS FUNCTION
+
+    session1good = session_good_clusters(cellDB, session1, tetrode)
+    session2good = session_good_clusters(cellDB, session2, tetrode)
+
+    #Make a boolean matrix from the two quality vectors, with 1 only if both are 1
+    #DONE: Which should come first for the across session comparisons?
+    #session 1 becomes the rows, which is the same as the corr function
+    qualityMat = np.outer(session1good.astype(int), session2good.astype(int)).astype(bool)
+
+    return qualityMat
+
+def triangle_filter(qualityMat):
+
+    #For the self analysis, we want to exclude the diagonal and the bottom half of the
+    #triangle
+    #We use the -1th diagonal so that the self comparisons are filtered out
+    qualityMat = np.tril(qualityMat, k=-1)
+    return qualityMat
+
+def ISI_filter(subject, session1, session2, tetrode):
+    '''
+    Read isi violations for each session and filter for comparisons between
+    cells that do not violate
+
+    '''
+    pass
+
+def print_reports_clusters(subject, sessions, tetrode, printer):
+    '''
+    use lpstat -p to find printers
+    use lpadmin -p printername to add a printer as the default
+    '''
+
+    reportsDir = os.path.join(settings.EPHYS_PATH, subject, 'reports_clusters')
+    for session in sessions:
+        reportFilename = '{}_{}_T{}.png'.format(subject, session, tetrode)
+        fullReportPath = os.path.join(reportsDir, reportFilename)
+        printcommand = ['lpr', '-P {}'.format(printer), fullReportPath]
+        print ' '.join(printcommand)
+        # subprocess.call(printcommand)
+
+
 
 if __name__=='__main__':
     ### Useful trick:   np.set_printoptions(linewidth=160)
-    CASE = 0
+    CASE = 3
     if CASE==0:
         nSamples = 4*40
         nClusters = 12
@@ -165,3 +275,144 @@ if __name__=='__main__':
         #(ccSelf,ccAccross) = spikeshape_correlation([awave])
         #plot_correlation(ccSelf,ccAccross)
 
+    elif CASE==2:
+
+        #Load the waveforms for all the sessions and save them
+        subject = 'test098'
+        sessions = ['2016-07-26_12-18-39','2016-07-26_12-30-36']
+        tetrode=1
+        sessionWaves = waveforms_many_sessions(subject, sessions, tetrode)
+
+        #Save the list of waveform arrays as compressed binary file
+        waveFile = '/tmp/{}waves.npz'.format(subject)
+        np.savez_compressed(waveFile, **dict(zip(sessions, sessionWaves)))
+
+        #Read the saved waveforms back in as a list
+        #TODO: This returns a dict, which may not be sorted
+        arrFile = np.load(waveFile)
+        loadWaves = [arr for name, arr in arrFile.items()]
+        loadSessions = [name for name, arr in arrFile.items()]
+
+        ccSelf, ccAcross = spikeshape_correlation(loadWaves)
+
+        corrFile = '/tmp/{}corr.npz'.format(subject)
+        np.savez_compressed(corrFile, ccSelf=ccSelf, ccAcross=ccAcross)
+
+        loadCorr = np.load(corrFile)
+        plot_correlation(loadCorr['ccSelf'], loadCorr['ccAcross'],'viridis')
+
+    elif CASE==3:
+
+        from jaratest.billy.scripts import celldatabase_quality_tuning as cellDB
+        import sys
+        import importlib
+        import subprocess
+
+        ##
+        subject = 'adap017'
+        tetrodes = range(1, 9)
+        threshold = 0.7
+        ##
+
+        ### -- DO THIS PER ANIMAL -- ###
+        #Get the allcells file
+        allcellsFilename = 'allcells_{}_quality'.format(subject)
+        sys.path.append(settings.ALLCELLS_PATH)
+
+        allcells = importlib.import_module(allcellsFilename)
+
+        #Find all the sessions for this allcells file
+        sessions = find_ephys_sessions(allcells.cellDB)
+
+        clusterDirs = ['{}_kk'.format(session) for session in sessions]
+
+        #Rsync the data from jarastore for these sessions if needed
+        for session in sessions:
+            rsync_session_data(subject, session, skipIfExists=True)
+        for clusterDir in clusterDirs:
+            rsync_session_data(subject, clusterDir, skipIfExists=True)
+
+        ### -- DO THIS PER TETRODE -- ###
+
+        for tetrode in tetrodes:
+            #If the waves exist, load them. If not, get them
+            waveFile = '/tmp/{}_TT{}waves.p'.format(subject, tetrode)
+
+            # #DONE: stop calculating waves every time
+            if os.path.isfile(waveFile):
+                #Load the waves
+                print "Loading average waves for {} tetrode {}".format(subject, tetrode)
+                sessionWaves = pickle.load(open(waveFile, 'rb'))
+            else:
+                print "Calculating average waves for Subject {} tetrode {}".format(subject, tetrode)
+                sessionWaves = waveforms_many_sessions(subject, sessions, tetrode)
+                pickle.dump(sessionWaves, open(waveFile, 'wb'))
+
+            #TODO: if the correlations exist we don't need to load the waves
+            corrFile = '/tmp/{}_TT{}corr.npz'.format(subject, tetrode)
+            if os.path.isfile(corrFile):
+                #Load the correlations if they already exist
+                loadCorr = np.load(corrFile)
+                ccSelf = loadCorr['ccSelf']
+                ccAcross = loadCorr['ccAcross']
+            else:
+                #Calculate the pairwise correlation between spikes
+                ccSelf, ccAcross = spikeshape_correlation(sessionWaves)
+                #Save the correlations
+                np.savez_compressed(corrFile, ccSelf=ccSelf, ccAcross=ccAcross)
+
+            #Loop self comparisons WITH THRESHOLD and save to file
+            #DONE: Name the file according to animal and tetrode
+            allSelfCorrVals = []
+            allSelfCorrComps = []
+            f = open('/tmp/{}_TT{}_SELF_{}thresh.txt'.format(subject, tetrode, threshold), 'w')
+            header = '{}  TT{}  SELF correlation'.format(subject, tetrode)
+            f.write(header)
+            f.write('\n')
+            for indSession, session in enumerate(sessions):
+                selfCompThisSession = ccSelf[indSession]
+                qualityMat = comparison_quality_filter(allcells.cellDB, session, session, tetrode)
+                qualityMat = triangle_filter(qualityMat)
+                larray = comparison_label_array(session, session, tetrode)
+                goodCorrVals = selfCompThisSession[qualityMat]
+                goodCompLabs = larray[qualityMat]
+                corrAboveThreshold = goodCorrVals[goodCorrVals>threshold]
+                compsAboveThreshold = goodCompLabs[goodCorrVals>threshold]
+                #save out the values
+                allSelfCorrVals.extend(list(goodCorrVals))
+                message = '\n##------------## Session {} Self Comparison ##------------##\n'.format(session)
+                f.write(message)
+                # for corr in compsAboveThreshold:
+                #     f.write(corr)
+                #     f.write('\n')
+                for ind in np.argsort(corrAboveThreshold)[::-1]:
+                    f.write('{0:.2f} - '.format(corrAboveThreshold[ind]))
+                    f.write(compsAboveThreshold[ind])
+                    f.write('\n')
+            f.close()
+
+            #Loop cross session comparisons no threshold
+            #DONE: Name the file according to animal and tetrode
+            allCrossCorrVals = []
+            allCrossCorrComps = []
+            f = open('/tmp/{}_TT{}_CROSS_{}thresh.txt'.format(subject, tetrode, threshold), 'w')
+            header = '{}  TT{}  CROSS correlation'.format(subject, tetrode)
+            f.write(header)
+            f.write('\n')
+            for indComp, (session1, session2) in enumerate(zip(sessions, sessions[1:])):
+                crossCompTheseSessions = ccAcross[indComp]
+                qualityMat = comparison_quality_filter(allcells.cellDB, session1, session2, tetrode)
+                larray = comparison_label_array(session1, session2, tetrode)
+                goodCorrVals = crossCompTheseSessions[qualityMat]
+                goodCompLabs = larray[qualityMat]
+                corrAboveThreshold = goodCorrVals[goodCorrVals>threshold]
+                compsAboveThreshold = goodCompLabs[goodCorrVals>threshold]
+                #save out the values
+                allCrossCorrVals.extend(list(goodCorrVals))
+                message = '\n##------Cross Comp {} x {}------##\n'.format(session1, session2)
+                f.write(message)
+                for ind in np.argsort(corrAboveThreshold)[::-1]:
+                    f.write('{0:.2f} - '.format(corrAboveThreshold[ind]))
+                    f.write(compsAboveThreshold[ind])
+                    f.write('\n')
+            f.close()
