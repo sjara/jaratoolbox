9021090570bc37605c9d681b8bd09f2985dbb6e5
Santiago Jaramillo <sjara@uoregon.edu>
2017-01-11 15:08:22 -0800

    Added variables to settings.

--- settings_EXAMPLE.py
+++ settings_EXAMPLE.py
@@ -12,3 +12,11 @@ EPHYS_PATH = '/data/ephys'
 DEFAULT_EXPERIMENTER = 'santiago'
 
 KK_PATH = '/home/jarauser/src/klustakwik/KK2/KlustaKwik'
+
+BEHAVIOR_PATH_REMOTE = '/mnt/jarahubdata/behavior'
+
+EPHYS_PATH_REMOTE = '/mnt/jarahubdata/ephys'
+
+VIDEO_PATH = '/mnt/jarahubdata/video'
+
+FIGURES_DATA_PATH = '/mnt/jarahubdata/figuresdata' # Intermediate data for subfigures
