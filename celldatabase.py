51ec71a3ebf9533213ad84416046c00f4e5c659f
Nick Ponvert <nickponvert@gmail.com>
2017-02-15 15:54:16 -0800

    Added function for generating a cell database to celldatabase module

--- celldatabase.py
+++ celldatabase.py
@@ -7,6 +7,7 @@ import numpy as np
 import os
 from jaratoolbox import settings
 import pandas as pd
+import imp
 
 class EphysSessionInfo(object):
      def __init__(self, animalName, ephysSession, behavSession,
@@ -343,7 +344,6 @@ class Site(object):
                'brainarea': self.brainarea,
                'info': self.info,
                'depth':self.depth,
-               'tetrodes':self.tetrodes,
                'ephys':self.session_ephys_dirs(),
                'behavior':self.session_behav_filenames(),
                'sessiontype':self.session_types()
@@ -425,6 +425,44 @@ def load_dataframe_from_HDF5(path, **kwargs):
     dataframe = pd.read_hdf(path, key='dataframe', **kwargs)
     return dataframe
 
+def generate_cell_database(inforecPath):
+    '''
+    Iterates over all sites in an inforec and builds a cell database
+    Args:
+        inforecPath (str): absolute path to the inforec file
+    Returns:
+        db (pandas.DataFrame): the cell database
+    '''
+
+    inforec = imp.load_source('module.name', inforecPath)
+    db = pd.DataFrame()
+    for indExperiment, experiment in enumerate(inforec.experiments):
+        for indSite, site in enumerate(experiment.sites):
+            clusterDir = 'multisession_exp{}site{}'.format(indExperiment, indSite)
+            for tetrode in site.tetrodes:
+                clusterStatsFn = 'Tetrode{}_stats.npz'.format(tetrode)
+                clusterStatsFullPath = os.path.join(settings.EPHYS_PATH,
+                                                    inforec.subject,
+                                                    clusterDir,
+                                                    clusterStatsFn)
+                if not os.path.isfile(clusterStatsFullPath):
+                    raise NotClusteredYetError("Experiment {} Site {} Tetrode {} is not clustered".format(indExperiment, indSite, tetrode))
+                clusterStats = np.load(clusterStatsFullPath)
+
+                for indc, cluster in enumerate(clusterStats['clusters']):
+                    clusterDict = {'tetrode':tetrode,
+                                   'cluster':cluster,
+                                   'indExperiment':indExperiment,
+                                   'indSite':indSite,
+                                   'inforecPath':inforecPath,
+                                   'nSpikes':clusterStats['nSpikes'][indc],
+                                   'isiViolations':clusterStats['isiViolations'][indc]}
+                    clusterDict.update(site.cluster_info())
+                    db = db.append(clusterDict, ignore_index=True)
+    return db
+
+class NotClusteredYetError(Exception):
+    pass
 
 if __name__=="__main__":
      CASE=0
