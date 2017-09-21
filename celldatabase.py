8f4874916d26be648298850a48844620d92f4f94
Nick Ponvert <nickponvert@gmail.com>
2017-09-21 13:35:04 -0700

    Added functions for getting session behavior/ephys for individual cells to celldatabase.

--- celldatabase.py
+++ celldatabase.py
@@ -5,7 +5,10 @@
 
 import numpy as np
 import os
+import sys
 from jaratoolbox import settings
+from jaratoolbox import loadbehavior
+from jaratoolbox import loadopenephys
 import pandas as pd
 import imp
 
@@ -614,3 +617,109 @@ if __name__=="__main__":
         df2 = load_dataframe_from_HDF5('/tmp/df_test.h5')
         print type(df2['b'][0])
 
+def get_session_inds(cell, sessiontype):
+    '''
+    Get the index of a particular sessiontype for a cell.
+    Args:
+        cell (pandas.Series): One row from a pandas cell database created using generate_cell_database or by
+                              manually constructing a pandas.Series object that contains the required fields.
+                              This function requires the 'sessiontype' field, which must contain a list of strings.
+        sessiontype (str): The type of session
+
+    Returns:
+        sessionInds (list): A list of indices where cell['sessiontype'] and the sessiontype arg match.
+    '''
+    sessionInds = [i for i, st in enumerate(cell['sessiontype']) if st==sessiontype]
+    return sessionInds
+
+def get_session_bdata(cell, sessiontype):
+    '''
+    Load the behavior data from a cell for a single session.
+    Args:
+        cell (pandas.Series): One row from a pandas cell database created using generate_cell_database or by
+                              manually constructing a pandas.Series object that contains the required fields.
+                              This function requires the 'sessiontype' field, which must contain a list of strings.
+        sessiontype (str): The type of session
+
+    Returns:
+        bdata (jaratoolbox.loadbehavior.BehaviorData): The behavior data for the session
+    '''
+    sessionInds = get_session_inds(cell, sessiontype)
+    sessionInd = sessionInds[0] #FIXME: Just takes the first one for now
+    behavFile = cell['behavior'][sessionInd]
+    behavDataFilePath=os.path.join(settings.BEHAVIOR_PATH, cell['subject'], behavFile)
+    bdata = loadbehavior.BehaviorData(behavDataFilePath,readmode='full')
+    return bdata
+
+def get_session_ephys(cell, sessiontype):
+    '''
+    Load the spikes and events from a cell for a single session.
+    Args:
+        cell (pandas.Series): One row from a pandas cell database created using generate_cell_database or by
+                              manually constructing a pandas.Series object that contains the required fields.
+        sessiontype (str): The type of session
+    Returns:
+        spikeData (jaratoolbox.loadopenephys.DataSpikes): The spike data for the session
+        eventData (jaratoolbox.loadopenephys.Events): The event data for the session
+    '''
+    sessionInds = get_session_inds(cell, sessiontype)
+    sessionInd = sessionInds[0] #FIXME: Just takes the first one for now
+    ephysSession = cell['ephys'][sessionInd]
+    ephysBaseDir = os.path.join(settings.EPHYS_PATH, cell['subject'])
+    tetrode=int(cell['tetrode'])
+    eventFilename=os.path.join(ephysBaseDir,
+                               ephysSession,
+                               'all_channels.events')
+    spikesFilename=os.path.join(ephysBaseDir,
+                                ephysSession,
+                                'Tetrode{}.spikes'.format(tetrode))
+    eventData=loadopenephys.Events(eventFilename)
+    spikeData = loadopenephys.DataSpikes(spikesFilename)
+    if spikeData.timestamps is not None:
+        clustersDir = os.path.join(ephysBaseDir, '{}_kk'.format(ephysSession))
+        clustersFile = os.path.join(clustersDir,'Tetrode{}.clu.1'.format(tetrode))
+        spikeData.set_clusters(clustersFile)
+        spikeData.samples=spikeData.samples[spikeData.clusters==cell['cluster']]
+        spikeData.timestamps=spikeData.timestamps[spikeData.clusters==cell['cluster']]
+        spikeData = loadopenephys.convert_openephys(spikeData)
+    eventData = loadopenephys.convert_openephys(eventData)
+    return spikeData, eventData
+
+def load_all_spikedata(cell):
+    '''
+    Load the spike data for all recorded sessions into a set of arrays.
+    Args:
+        cell (pandas.Series): One row from a pandas cell database created using generate_cell_database or by
+                              manually constructing a pandas.Series object that contains the required fields.
+    Returns:
+        timestamps (np.array): The timestamps for all spikes across all sessions
+        samples (np.array): The samples for all spikes across all sessions
+        recordingNumber (np.array): The index of the session where the spike was recorded
+    '''
+
+    samples=np.array([])
+    timestamps=np.array([])
+    recordingNumber=np.array([])
+
+    for ind, sessionType in enumerate(cell['sessiontype']):
+        dataSpkObj, dataEvents = get_session_ephys(cell, sessionType)
+        if (dataSpkObj.timestamps is None) or (len(dataSpkObj.timestamps)==0):
+            continue
+
+        numSpikes = len(dataSpkObj.timestamps)
+        sessionVector = np.zeros(numSpikes)+ind
+
+        if len(samples)==0:
+            samples = dataSpkObj.samples
+            timestamps = dataSpkObj.timestamps
+            recordingNumber = sessionVector
+        else:
+            samples = np.concatenate([samples, dataSpkObj.samples])
+            # Check to see if next session ts[0] is lower than self.timestamps[-1]
+            # If so, add self.timestamps[-1] to all new timestamps before concat
+            if dataSpkObj.timestamps[0]<timestamps[-1]:
+                dataSpkObj.timestamps = dataSpkObj.timestamps + timestamps[-1]
+            timestamps = np.concatenate([timestamps, dataSpkObj.timestamps])
+            recordingNumber = np.concatenate([recordingNumber, sessionVector])
+
+    return timestamps, samples, recordingNumber
