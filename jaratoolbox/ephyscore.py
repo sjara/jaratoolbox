1bb1f5ff20cce91a5541983741ab07fbdc88792a
Santiago Jaramillo <sjara@uoregon.edu>
2021-11-15 19:21:13 -0800

    Major changes to enable use of neuropixels probes.

--- jaratoolbox/ephyscore.py
+++ jaratoolbox/ephyscore.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 import pandas as pd
 from jaratoolbox import loadopenephys
+from jaratoolbox import loadneuropix
 from jaratoolbox import loadbehavior
 from jaratoolbox import settings
 
@@ -18,37 +19,41 @@ CHANNELMAPS = {'threetones_sequence': {'stim':0, 'trialStart':1, 'laser':2, 'sou
                'bandwidth_am':{'stim':0, 'trialStart':1, 'laser':2, 'soundDetector':5},
                'twochoice':{'stim':0, 'trialStart':1, 'laser':2, 'soundDetector':5},
                'laser_tuning_curve':{'stim':0, 'trialStart':1, 'laser':2},
-               '2afc':{'stim':0, 'trialStart':1}}
+               '2afc':{'stim':0, 'trialStart':1},
+               '2afc_speech':{'stim':0, 'trialStart':1} }
 
-class Cell(object):
+class Cell():
     def __init__(self, dbRow, useModifiedClusters=False):
         '''
-        Things to check at the end:
-        * When looping through cells, make sure that nothing is preserved from iteration to the next
         Args:
-            dbRow (pandas.core.series.Series): A row from a dataframe. Must contain at least:
-            * sessionType
-            useModifiedClusters (bool): Whether to load the modified cluster files created after cleaning clusters, if they exist.
+            dbRow (pandas.core.series.Series): A row from a dataframe created by celldatabase.
+            useModifiedClusters (bool): Whether to load the modified cluster files created after
+                                        cleaning clusters, if they exist.
         '''
+        # FIXME: When looping through cells, make sure that nothing is
+        #        preserved from iteration to the next
         if not isinstance(dbRow, pd.core.series.Series):
             raise TypeError('This object must be initialized with a pandas Series object.')
         self.dbRow = dbRow
         self.useModifiedClusters = useModifiedClusters
 
-        # We use these variables many times to load data
-        # Date and depth are not really used to load the data
         self.subject = dbRow['subject']
         self.date = dbRow['date']
-        self.depth = dbRow['depth']
-        self.tetrode = dbRow['tetrode']
+        if 'pdepth' in dbRow:
+            self.pdepth = dbRow['pdepth']
+        else:
+            self.pdepth = dbRow['depth']  # Legacy option for celldatabase v3.0
+        if 'egroup' in dbRow:
+            self.egroup = dbRow['egroup']
+        else:
+            self.egrouptetrode = dbRow['tetrode']  # Legacy option for celldatabase v3.0
         self.cluster = dbRow['cluster']
         self.ephysBaseDir = os.path.join(settings.EPHYS_PATH, self.subject)
 
     def __str__(self):
-        objStr = '{} {} ({:0.0f}um) T{}c{}'.format(self.subject, self.date, self.depth,
-                                            self.tetrode, self.cluster)
+        objStr = '{} {} ({:0.0f}um) g{}c{}'.format(self.subject, self.date, self.pdepth,
+                                                   self.egroup, self.cluster)
         return objStr
-        
         
     def load(self, sessiontype, behavClass=None):
         '''
@@ -56,7 +61,8 @@ class Cell(object):
         session of the type that was recorded from the cell.
         Args:
             sessiontype (str): the type of session to load data for.
-            behavClass (jaratoolbox.loadbehavior Class): The loading class to use, each class of behavData will have different methods.
+            behavClass (jaratoolbox.loadbehavior Class): The loading class to use, each class
+                                                         of behavData will have different methods.
         Returns:
             ephysData (dict):'spikeTimes' (array), 'samples' (array) and 'events' (dict)
                               The dictionary 'events' contains two keys for each type of event
@@ -87,10 +93,12 @@ class Cell(object):
 
     def load_by_index(self, sessionInd, behavClass=None):
         '''
-        Load both ephys and behavior data for a session using the absolute index in the list of sessions for the cell.
+        Load both ephys and behavior data for a session using the absolute index in the list
+        of sessions for the cell.
         Args:
             sessionInd (int): The index of the session in the list of sessions recorded for the cell.
-            behavClass (jaratoolbox.loadbehavior Class): The loading class to use, each class of behavData will have different methods.
+            behavClass (jaratoolbox.loadbehavior Class): The loading class to use, each class of
+                                                         behavData will have different methods.
         Returns:
             ephysData (list): Spiketimes (array), samples (array) and events (dict)
             behavData (jaratoolbox.loadbehavior.BehaviorData): Behavior data dict
@@ -107,32 +115,37 @@ class Cell(object):
         Returns:
             ephysData (list): Spiketimes (array), samples (array) and events (dict)
         '''
-
-        (sessionDir, paradigm) = self.get_ephys_filename(sessionInd)
-        #TODO: Maybe sessionDir and paradigm should be a tuple?
-        ephysData = load_ephys(self.subject, paradigm, sessionDir, self.tetrode, self.cluster, useModifiedClusters=self.useModifiedClusters)
+        sessionDir = self.get_ephys_dir(sessionInd)
+        paradigm = self.dbRow['paradigm'][sessionInd]
+        if self.dbRow['probe'] == 'A4x2-tet':
+            ephysData = load_ephys_neuronexus_tetrodes(self.subject, paradigm, sessionDir,
+                                                       self.egroup, self.cluster,
+                                                       useModifiedClusters=self.useModifiedClusters)
+        elif self.dbRow['probe'][:4] == 'NPv1':
+            ephysData = load_ephys_neuropixels_v1(self.subject, paradigm, sessionDir,
+                                                  self.egroup, self.cluster)
         return ephysData
 
-    def get_ephys_filename(self, sessionInd):
+    def get_ephys_dir(self, sessionInd):
         '''
-        Return the full path for the .spikes and .events files for a session.
+        Return the full path to the ephys data for a given session.
         Args:
             sessionInd (int): The index of the session in the list of sessions recorded for the cell.
         Returns:
-            spikesFilename (str): Full path to the .spikes file
-            eventsFilename (str): Full path to the .events file
+            sessionDir (str): Full path to the ephys data
         '''
         ephysTime = self.dbRow['ephysTime'][sessionInd]
         sessionDir = '{}_{}'.format(self.date, ephysTime)
-        paradigm = self.dbRow['paradigm'][sessionInd]
-        return (sessionDir, paradigm)
+        return sessionDir
 
     def load_behavior_by_index(self, sessionInd, behavClass=None):
         '''
-        Load the behavior data for a session using the absolute index in the list of sessions for the cell.
+        Load the behavior data for a session using the absolute index in the list of sessions
+        for the cell.
         Args:
             sessionInd (int): The index of the session in the list of sessions recorded for the cell.
-            behavClass (jaratoolbox.loadbehavior Class): The loading class to use, each class of behavData will have different methods.
+            behavClass (jaratoolbox.loadbehavior Class): The loading class to use, each class of
+                                                         behavData will have different methods.
         Returns:
             behavData (jaratoolbox.loadbehavior.BehaviorData): Behavior data dict
 
@@ -149,8 +162,8 @@ class Cell(object):
             dateStr = ''.join(self.date.split('-'))
             fullSessionStr = '{}{}'.format(dateStr, self.dbRow['behavSuffix'][sessionInd])
             behavDataFilePath = loadbehavior.path_to_behavior_data(self.subject,
-                                                                self.dbRow['paradigm'][sessionInd],
-                                                                fullSessionStr)
+                                                                   self.dbRow['paradigm'][sessionInd],
+                                                                   fullSessionStr)
             bdata = behavClass(behavDataFilePath,readmode='full')
         else:
             bdata = None
@@ -192,30 +205,20 @@ class Cell(object):
         recordingNumber = recordingNumber.astype(int)
         return timestamps, samples, recordingNumber
 
-# spikesFilename, eventsFilename = self.get_ephys_filename(sessionInd)
-# paradigm = self.dbRow['paradigm'][sessionInd]
-# #Base, Folder, spikesFn (Need tetrode), eventsFn (always the same)
-# #Really need base (settings + subject), folder, tetrode
 
-# #clustersDir = Base + Folder_kk + clustersFn
-
-# clustersDir = os.path.join(self.ephysBaseDir, '{}_{}_kk'.format(self.dbRow['date'],
-                                                                    # self.dbRow['ephysTime'][sessionInd]))
-
-def load_ephys(subject, paradigm, sessionDir, tetrode, cluster=None, useModifiedClusters=False):
-
-    #Setup path and filenames
+def load_ephys_neuronexus_tetrodes(subject, paradigm, sessionDir, tetrode, cluster=None,
+                                   useModifiedClusters=False):
+    # -- Setup path and filenames --
     ephysBaseDir = os.path.join(settings.EPHYS_PATH, subject)
     eventsFilename = os.path.join(ephysBaseDir, sessionDir,
                                     'all_channels.events')
     spikesFilename = os.path.join(ephysBaseDir, sessionDir,
                                     'Tetrode{}.spikes'.format(tetrode))
-
-    #Load the spikes and events
+    # -- Load the spikes and events --
     eventData = loadopenephys.Events(eventsFilename)
     spikeData = loadopenephys.DataSpikes(spikesFilename)
 
-    #Fail if there are no spikes for the tetrode
+    # -- Fail if there are no spikes for the tetrode --
     if spikeData.timestamps is None:
         raise ValueError('File {} contains no spikes.'.format(spikesFilename))
 
@@ -234,17 +237,18 @@ def load_ephys(subject, paradigm, sessionDir, tetrode, cluster=None, useModified
                 print("Loading modified .clu file for session {}".format(spikesFilename))
                 spikeData.set_clusters(clustersFileModified)
             else:
-                print("Modified .clu file does not exist, loading standard .clu file for session {}".format(spikesFilename))
+                print(f'Modified .clu file does not exist, loading standard .clu file ' +
+                      'for session {spikesFilename}')
                 clustersFile = os.path.join(clustersDir,'Tetrode{}.clu.1'.format(tetrode))
                 spikeData.set_clusters(clustersFile)
         spikeData.samples = spikeData.samples[spikeData.clusters==cluster]
         spikeData.timestamps = spikeData.timestamps[spikeData.clusters==cluster]
 
-    #Convert to real units
+    # -- Convert to standard units --
     spikeData = loadopenephys.convert_openephys(spikeData)
     eventData = loadopenephys.convert_openephys(eventData)
 
-    #Choose channel map based on paradigm
+    # -- Choose channel map based on paradigm --
     channelMap = CHANNELMAPS[paradigm]
     eventDict = {}
     for channelType, channelID in channelMap.items():
@@ -257,3 +261,27 @@ def load_ephys(subject, paradigm, sessionDir, tetrode, cluster=None, useModified
                     'events':eventDict}
     return ephysData
 
+
+def load_ephys_neuropixels_v1(subject, paradigm, sessionDir, egroup, cluster=None):
+    ephysBaseDir = os.path.join(settings.EPHYS_NEUROPIX_PATH, subject)
+    eventsDir = os.path.join(ephysBaseDir, sessionDir)
+    spikesDir = os.path.join(ephysBaseDir, sessionDir+'_processed') # This may fail if ends in /
+
+    # -- Load spikes and events --
+    spikesData = loadneuropix.Spikes(spikesDir, convert=True)
+    eventsData = loadneuropix.Events(eventsDir, convert=True)
+    eventOnsetTimes = eventsData.get_onset_times()
+
+    # -- Find events on each channel of the channel map for this paradigm --
+    channelMap = CHANNELMAPS[paradigm]
+    eventDict = {}
+    for channelType, channelID in channelMap.items():
+        channelID = channelID+1   # FIXME: it looks like neuropix system uses Channel 1 not 0
+        thisChannelOn = eventsData.get_onset_times(channelID, channelState=1)
+        thisChannelOff = eventsData.get_onset_times(channelID, channelState=-1)
+        eventDict.update({'{}On'.format(channelType):thisChannelOn,
+                          '{}Off'.format(channelType):thisChannelOff})
+    ephysData = {'spikeTimes': spikesData.get_timestamps(cluster),
+                 'events': eventDict}
+    return ephysData
+ 
