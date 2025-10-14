cd4c707b6e999f750a2f451f40d7374f8e048dfa
sjara <sjara@uoregon.edu>
2025-10-14 11:32:53 -0700

    Added script to run kilosort with the correct probe map.

---
+++ scripts/neuropix_kilosort.py
@@ -0,0 +1,77 @@
+"""
+Spike-sort Neuropixels data using Kilosort 4.
+"""
+
+import os
+import sys
+import importlib
+import numpy as np
+from jaratoolbox import settings
+from jaratoolbox import loadneuropix
+
+
+scriptName = os.path.basename(__file__)
+
+if len(sys.argv)<4:
+    print('This command requires a subject, date and probe depth as arguments.\n'+
+          f'Example: {scriptName} test000 2020-01-31 3440')
+    sys.exit()
+
+subject = sys.argv[1]
+dateStr = sys.argv[2]
+pdepth = int(sys.argv[3])
+debug = True if (len(sys.argv)==5 and sys.argv[4]=='debug') else False
+
+sessionsRootPath = os.path.join(settings.EPHYS_NEUROPIX_PATH, subject)
+multisessionRawDir = os.path.join(sessionsRootPath, f'multisession_{dateStr}_{pdepth}um_raw')
+multisessionProcessedDir = os.path.join(sessionsRootPath, f'multisession_{dateStr}_{pdepth}um_processed')
+rawFilename = os.path.join(multisessionRawDir , 'multisession_continuous.dat')
+
+# -- Load inforec file --
+inforecFile = os.path.join(settings.INFOREC_PATH, f'{subject}_inforec.py')
+spec = importlib.util.spec_from_file_location('inforec_module', inforecFile)
+inforec = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(inforec)
+
+# -- Find sessions for this site --
+siteToProcess = None
+for experiment in inforec.experiments:
+    if experiment.date==dateStr:
+        for site in experiment.sites:
+            if site.pdepth==pdepth:
+                probeStr = experiment.probe
+                siteToProcess = site
+if siteToProcess is None:
+    print(f'Recording for {subject} on {dateStr} at {pdepth}um not found.')
+    sys.exit()
+sessions = siteToProcess.session_ephys_dirs()
+
+# -- Get probe map --
+xmlpath = os.path.join(multisessionProcessedDir , sessions[0] , 'info' , 'settings.xml')
+pmap = loadneuropix.ProbeMap(xmlpath)
+
+probe = {
+        'chanMap': pmap.channelID,
+        'xc': pmap.xpos,
+        'yc': pmap.ypos,
+        'kcoords': np.zeros(pmap.nActiveChannels)
+    }
+
+# -- Run kilosort --
+settings = {'n_chan_bin': pmap.nActiveChannels}
+
+if not debug:
+    ops, st, clu, tF, Wall, similar_templates, is_ref, est_contam_rate, kept_spikes = \
+        run_kilosort(settings=settings,
+                     filename=rawFilename,
+                     probe_name=pmap.probeName,
+                     probe=probe,
+                     results_dir=multisessionProcessedDir)
+else:
+    print('This script will run Kilosort with the following parameters:')
+    print(f'Number of channels: {pmap.nActiveChannels}')
+    print(f'Probe name: {pmap.probeName}')
+    print(f'Raw data file: {rawFilename}')
+    print(f'Results directory: {multisessionProcessedDir}')
+print('Since you are running in debug mode, no files wwere created or modified.')
+
